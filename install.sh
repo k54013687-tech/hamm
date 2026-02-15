cf8514b7587f947ac5f576e2dd5112053a63e3a3
JUNED-404 <k54013687@gmail.com>
2026-02-15 23:13:12 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,53 @@
+N='\033[0m'
+R='\033[0;31m'
+G='\033[0;32m'
+O='\033[0;33m'
+P="\033[2m"
+BL='\033[0;96m\033[2m'
+B='\033[1;36m'
+BR="\033[0;34m"
+U="\033[0;35m"
+
+clear
+echo -e "\033[7;31m Instalation Module ${N}${R} FINAL${N}${G}""
+       　　　/)        /  
+     　　_(⌒)＿　　/)            instalation Module
+     　／ / ﾉ　 ヽ /ﾋE)          |==┣▇▇▇▇▇▇═─-SQl-i
+     `/ｲ// /LLﾄLL|/ /           Sabar Ya Bree
+     ｜|/ /(6　6(/ /             ./Juned
+     ｜/_/ " ヮ"ﾉ_/
+     /Y　ﾚ `ーイ /
+     ﾚ|　ヽ-====-＼
+     ﾚヽ　/／⌒＼⌒＼
+     　 ＼ｿ 　　｜  l
+     　　 )ヽ＿／＿／
+     　 ／　　 ﾉ 
+"
+#=== install module bre ===#
+pip install websockets
+pip install asyncio
+pip install colorama
+pip install aiohttp
+pip install threading
+pip install socket
+pip install collections
+pip install sys
+pip install urlibparse
+pip install json
+
+banner5="
+   ${G}    .---.        .----------- ${N}
+  ${G}    /     \  __  /    ------ ${N}
+   ${G}  / /     \(  )/    -----  ${N}   \033[7;36m Juned Ddos Tools ${N}\033[1;36m\033[1m Full${N}
+  ${G}  //////   ' \/ \`   ---   ${N}    \033[2m |==┣▇▇▇▇▇▇▇▇═─-SQl-i ${N}
+${G}   //// / // :    : ---   ${N}       ${R}Agressiv${N}Requests${N}
+${G}  // /   /  /\`    '--     ${N}      ${O}./Juned${O}3${O}n${N} 
+${G} //          //..\\${N}
+    ${G}    ====UU====UU====
+            '//||\\\`
+              ''\`\`
+        ${R}[✓] Module Berhasil di Install ${N}"
+        
+echo -e "\033[7;31m[+] Selesai Install Module, Menuju ke tools$N"
+sleep(2)
+python3 juned_main.py
